7bc6c2d7ff9beafbc3d21074e0bb37510b98c3c5
João Parreira <joao.parreira@mousquetaires.com>
2022-01-11 10:21:34 +0000

    build cjs

--- build.cjs
+++ build.cjs
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-//const fs = require("fs");
-//const path = require("path");
+const fs = require("fs");
+const path = require("path");
 
-import * as fs from "fs";
-import * as path from "path";
+//import * as fs from "fs";
+//import * as path from "path";
 
 fs.writeFileSync(path.join("./", "dist", "mjs", "package.json"), JSON.stringify({ "type": "module" }, null, 4));
 fs.writeFileSync(path.join("./", "dist", "cjs", "package.json"), JSON.stringify({ "type": "commonjs" }, null, 4));
